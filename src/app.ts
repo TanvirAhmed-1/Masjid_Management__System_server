abad14642cfce256b6e48305e2c5dee41b3945a1
Tanvir Ahmed <tanvirahmed1078@gmail.com>
2026-04-11 18:56:41 +0600

    fix

--- src/app.ts
+++ src/app.ts
@@ -9,17 +9,37 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-const allowedOrigins = [
-  "http://localhost:3000",
-  "https://masjid-management-gules.vercel.app",
-];
+// const allowedOrigins = [
+//   "http://localhost:3000",
+//   "https://masjid-management-gules.vercel.app",
+// ];
+
+// app.use(
+//   cors({
+//     origin: allowedOrigins,
+//     credentials: true,
+//   }),
+// );
+
 
 app.use(
   cors({
-    origin: allowedOrigins,
+    origin: (origin, callback) => {
+      const allowedOrigins = [
+        "http://localhost:3000",
+        "https://masjid-management-gules.vercel.app",
+      ];
+
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
-  }),
+  })
 );
+
 
 //  Routes
 app.use("/api", BaseRouter);
